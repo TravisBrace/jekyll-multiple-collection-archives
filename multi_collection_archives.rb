b134d8e049b85c7c8cb06bbb5148df8db851e078
Travis Brace <TravisBrace@gmail.com>
2023-08-21 09:50:45 -0400

    updated wrong plugin code

--- multi_collection_archives.rb
+++ multi_collection_archives.rb
@@ -1,59 +1,67 @@
-# _plugins/category_pages.rb
 module Jekyll
-  class CategoryPageGenerator < Generator
+  class CollectionArchiveGenerator < Generator
     safe true
 
-    def slugify(category)
-      category.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+    def generate(site)
+      collections_to_archive = site.config['collections_with_archives'] || []
+
+      collections_to_archive.each do |collection_config|
+        collection_name = collection_config['name']
+        layout = collection_config['layout']
+        archive_types = collection_config['archive_type'] || []
+        monthly_archive_slug = collection_config['monthly_archive_slug'] || "/blog/:collection_name/monthly/:year/:month/"
+        yearly_archive_slug = collection_config['yearly_archive_slug'] || "/blog/:collection_name/yearly/:year/"
+
+        if site.collections.key?(collection_name)
+          collection = site.collections[collection_name]
+
+          if archive_types.include?('monthly')
+            # Generate monthly archives
+            posts_grouped_by_month(site, collection).each do |period, posts|
+              year, month = period.split('-')
+              archive_dir = monthly_archive_slug.gsub(':collection_name', collection_name)
+                                                .gsub(':year', year)
+                                                .gsub(':month', month)
+              site.pages << CollectionArchive.new(site, site.source, archive_dir, period, posts, collection_name, layout)
+            end
+          end
+
+          if archive_types.include?('yearly')
+            # Generate yearly archives
+            posts_grouped_by_year(site, collection).each do |year, posts|
+              archive_dir = yearly_archive_slug.gsub(':collection_name', collection_name)
+                                               .gsub(':year', year.to_s)
+              site.pages << CollectionArchive.new(site, site.source, archive_dir, year.to_s, posts, collection_name, layout)
+            end
+          end
+
+        else
+          Jekyll.logger.warn "Warning:", "Collection '#{collection_name}' not found. Skipping archive generation."
+        end
+      end
     end
 
-    def generate(site)
-      category_configs = site.config['category_pages'] || []
+    def posts_grouped_by_month(site, collection)
+      collection.docs.group_by { |post| post.date.strftime('%Y-%m') }
+    end
 
-      category_configs.each do |config|
-        collection_name = config['collection']
-        collection = site.collections[collection_name]
-        next if collection.nil?
-
-        url_template = config['url_structure']
-        layout = config['layout']
-
-        category_pages = {}
-
-        collection.docs.each do |doc|
-          categories = Array(doc.data['category'])
-          categories += Array(doc.data['categories'])
-          categories.uniq!
-          categories.compact!
-        
-          categories.each do |category|
-            category_slug = slugify(category)
-            category_pages[category_slug] ||= CategoryPage.new(site, collection, category, url_template, layout)
-            category_pages[category_slug].data['items'] << doc
-            category_pages[category_slug].data['titletag'] = config['seo_title'].gsub(':category', category)
-            category_pages[category_slug].data['description'] = config['seo_description'].gsub(':category', category)
-          end
-        end
-
-        category_pages.each_value { |page| site.pages << page }
-      end
+    def posts_grouped_by_year(site, collection)
+      collection.docs.group_by { |post| post.date.year }
     end
   end
 
-  class CategoryPage < Page
-    def initialize(site, collection, category, url_template, layout)
+  class CollectionArchive < Page
+    def initialize(site, base, dir, period, posts, collection, layout)
       @site = site
-      @base = site.source
+      @base = base
+      @dir = dir
       @name = 'index.html'
 
       self.process(@name)
-      self.read_yaml(File.join(@base, '_layouts'), "#{layout}.html")
-
-      self.data['title'] = category
-      self.data['items'] = []
-
-      url = url_template.gsub(':collection', collection.label).gsub(':category', category.downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, ''))
-      self.data['permalink'] = url
+      self.read_yaml(File.join(base, '_layouts'), "#{layout}.html")
+      self.data['period'] = period
+      self.data['posts'] = posts
+      self.data['collection'] = collection
     end
   end
 end
